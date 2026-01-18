422452c654da151b603329820bb12062b2bbc32f
Papp Dávid <impappdavid@gmail.com>
2026-01-18 16:28:21 +0100

    Making the 6.14 part

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -2,9 +2,18 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
 
 const App = () => {
-  
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdote => dispatch(setAnecdotes(anecdote)))
+  }, [dispatch])
 
   return (
     <div>
